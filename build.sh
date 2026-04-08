ab94c9969bedf7c26907a9ab51346e88277bff36
agatav13 <agataomasta05@gmail.com>
2026-04-08 22:04:44 +0200

    update config

--- build.sh
+++ build.sh
@@ -4,3 +4,7 @@ set -o errexit
 uv sync --python 3.13
 uv run manage.py collectstatic --noinput
 uv run manage.py migrate
+
+if [ -n "$DJANGO_SUPERUSER_EMAIL" ]; then
+  uv run manage.py createsuperuser --noinput || true
+fi
